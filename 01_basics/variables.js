88ab781cf0776a8d7ce71e5dc6067f6c4bdc5da1
abhishek5173 <114069034+abhishek5173@users.noreply.github.com>
2023-11-08 07:31:06 +0000

    datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accountId=144553
+let accountEmail="abhi@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
